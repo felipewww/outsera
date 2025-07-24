37d4a0fc796fee4147e2b66c765228dc84f0affa
Felipe Barreiros <felipewww@outlook.com.br>
2025-07-24 00:04:08 -0300

    apply bootstrap and create base routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {Home} from './home/home';
+import {List} from './list/list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'list',
+    component: List
+  }
+];
